5da36d08c85e0cf93c4fab7d5f89047a5eb46f16
OSUmageed <mageed@oregonstate.edu>
2016-04-14 16:17:04 -0700

    I'm making good progress.  I'm trying to make the linear index work.  90% done.

--- GaussSeidel_ProcedureCUDA/GS_CUDA_main.cu
+++ GaussSeidel_ProcedureCUDA/GS_CUDA_main.cu
@@ -2,7 +2,6 @@
 
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
-#include "C:\Users\Philadelphia\Documents\1_SweptTimeResearch\CUDA Examples\cuda_by_example\common\book.h"
 #include <cuda.h>
 #include <device_functions.h>
 #include <cuda_runtime_api.h>
@@ -23,12 +22,13 @@ using namespace std;
 #define DIVISIONS 2048
 #define TOLERANCE 1e-5
 
-// 
+// Define constants for GPU constant memory.
 __constant__ double d_bc[4];
 __constant__ double d_guess;
 __constant__ double d_A;
 __constant__ double d_ds;
-__constant__ double d_d_coeff;
+__constant__ double d_coeff;
+__constant__ double d_numel;
 
 __device__ void *cornerSource (int BC1, int BC2, double *source)
 {
@@ -36,7 +36,7 @@ __device__ void *cornerSource (int BC1, int BC2, double *source)
     {
         if (BC2>0)
         {
-            source[0] = 2*d_d_coeff*(BC1 + BC2);
+            source[0] = 2*d_coeff*(BC1 + BC2);
             source[1] = -4*d_coeff;
         }
         else
@@ -57,167 +57,199 @@ __device__ void *cornerSource (int BC1, int BC2, double *source)
     }
 }
 
-__device__ void differencingOperation(double *active_half[][DIVISIONS/2], double *passive_half[][DIVISIONS/2])
+__device__ void differencingOperation(double *active_half, double *passive_half, int active_index, int turn)
 {
 	
     double d_coeff_p;
     double *source = new double[2];
-	for (int i = 0; i < rw; i++)
-            {
-                for (int j = (m+i)%2; j < cl; j+=2)
-                {
-                    if (i == 0)
-                    {
-                        if (j == 0)
-                        {
-                            cornerSource(BC[2],BC[3],d_coeff, source);
-                            d_coeff_p = 2*d_coeff-source[1];
-                            t_matrix_final[i][j] = (d_coeff*(t_matrix_final[i+1][j]+t_matrix_final[i][j+1])+source[0])/d_coeff_p;
-                        }
-                        else if (j == (cl-1))
-                        {
-                            cornerSource(BC[2],BC[1],d_coeff, source);
-                            d_coeff_p = 2*d_coeff-source[1];
-                            t_matrix_final[i][j] = (d_coeff*(t_matrix_final[i+1][j]+t_matrix_final[i][j-1])+source[0])/d_coeff_p;
-                        }
-                        else
-                        {
-                            if (BC[2]>0)
-                            {
-                                source[0] = 2*d_coeff*BC[2];
-                                source[1] = -2*d_coeff;
-                                d_coeff_p = 3*d_coeff-source[1];
-                                t_matrix_final[i][j] = (d_coeff*(t_matrix_final[i][j+1]+t_matrix_final[i+1][j]+t_matrix_final[i][j-1])+source[0])/d_coeff_p;
-                            }
-                            else
-                            {
-                                d_coeff_p = 3*d_coeff;
-                                t_matrix_final[i][j] = d_coeff*(t_matrix_final[i+1][j]+t_matrix_final[i][j-1]+t_matrix_final[i][j+1])/d_coeff_p;
+	// Negative seq means active half starts second.  Positive seq means active half starts first.
+	int seq = pow(-1,turn+blockIdx.x);
 
-                            }
-                        }
-                    }
-                    else if (j == 0)
-                    {
-                        if (i == (rw-1))
-                        {
-                            cornerSource(BC[0],BC[3],d_coeff, source);
-                            d_coeff_p = 2*d_coeff-source[1];
-                            t_matrix_final[i][j] = (d_coeff*(t_matrix_final[i-1][j] + t_matrix_final[i][j+1])+source[0])/d_coeff_p;
-                        }
-                        else
-                        {
-                            if (BC[3]>0)
-                            {
-                                source[0] = 2*d_coeff*BC[3];
-                                source[1] = -2*d_coeff;
-                                d_coeff_p = 3*d_coeff-source[1];
-                                t_matrix_final[i][j] = (d_coeff*(t_matrix_final[i-1][j] + t_matrix_final[i+1][j] + t_matrix_final[i][j+1])+source[0])/d_coeff_p;
-                            }
-                            else
-                            {
-                                d_coeff_p = 3*d_coeff;
-                                t_matrix_final[i][j] = d_coeff*(t_matrix_final[i+1][j]+t_matrix_final[i-1][j]+t_matrix_final[i][j+1])/d_coeff_p;
+	// If bottom row.
+	if (active_index < blockDim.x)
+	{
+		// If bottom left (SouthWest) corner and red.
+		if (active_index == 0 && turn == 1)
+		{
+			cornerSource(d_bc[2],d_bc[3], source);
+			d_coeff_p = 2*d_coeff-source[1];
+			active_half[active_index] = (d_coeff*(passive_half[active_index]+passive_half[blockDim.x])+source[0])/d_coeff_p;	
+		}
+		// If bottom right (SouthEast) corner and black.
+		else if (active_index == (blockDim.x-1) && turn == 2)
+		{
+			cornerSource(d_bc[2],d_bc[1], source);
+			d_coeff_p = 2*d_coeff-source[1];
+			active_half[active_index] = (d_coeff*(passive_half[active_index]+passive_half[active_index+blockDim.x])+source[0])/d_coeff_p;
+		}
+		// Bottom row no corner.
+		else
+		{
+			// Check South Boundary Condition.  If it's constant temperature:
+			if (d_bc[2]>0)
+			{
+				source[0] = 2*d_coeff*d_bc[2];
+				source[1] = -2*d_coeff;
+				d_coeff_p = 3*d_coeff-source[1];
+				active_half[active_index] = d_coeff*(passive_half[active_index]+passive_half[active_index+seq]+passive_half[active_index+blockDim.x])/d_coeff_p;
+			}
+			else
+			{
+				d_coeff_p = 3*d_coeff;
+				active_half[active_index] = d_coeff*(passive_half[active_index]+passive_half[active_index+seq]+passive_half[active_index+blockDim.x])/d_coeff_p;
 
-                            }
-                        }
-                    }
-                    else if (i == (rw-1))
-                    {
-                        if (j == (cl-1))
-                        {
-                            cornerSource(BC[0], BC[1], d_coeff, source);
-                            d_coeff_p = 2*d_coeff-source[1];
-                            t_matrix_final[i][j] = (d_coeff*(t_matrix_final[i-1][j]+t_matrix_final[i][j-1])+source[0])/d_coeff_p;
+			}
+		}
+	}
+	// If top row
+	else if (active_index > d_numel-blockDim.x)
+	{
+		// If top right (NorthEast) corner and red.
+		if (active_index == (d_numel-1) && turn == 1)
+		{
+			cornerSource(d_bc[0],d_bc[1], source);
+			d_coeff_p = 2*d_coeff-source[1];
+			active_half[active_index] = (d_coeff*(passive_half[active_index]+passive_half[active_index-blockDim.x])+source[0])/d_coeff_p;	
+		}
+		// If top left (NorthWest) corner and black.
+		else if (active_index == (d_numel-(blockDim.x-1)) && turn == 2)
+		{
+			cornerSource(d_bc[0],d_bc[3], source);
+			d_coeff_p = 2*d_coeff-source[1];
+			active_half[active_index] = (d_coeff*(passive_half[active_index]+passive_half[active_index+blockDim.x])+source[0])/d_coeff_p;
+		}
+		// Top row no corner.
+		else
+		{
+			// Check South Boundary Condition.  If it's constant temperature:
+			if (d_bc[2]>0)
+			{
+				source[0] = 2*d_coeff*d_bc[2];
+				source[1] = -2*d_coeff;
+				d_coeff_p = 3*d_coeff-source[1];
+				active_half[active_index] = d_coeff*(passive_half[active_index]+passive_half[active_index+seq]+passive_half[active_index+blockDim.x])/d_coeff_p;
+			}
+			else
+			{
+				d_coeff_p = 3*d_coeff;
+				active_half[active_index] = d_coeff*(passive_half[active_index]+passive_half[active_index+seq]+passive_half[active_index+blockDim.x])/d_coeff_p;
 
-                        }
-                        else
-                        {
-                            if (BC[0]>0)
-                            {
-                                source[0] = 2*d_coeff*BC[0];
-                                source[1] = -2*d_coeff;
-                                d_coeff_p = 3*d_coeff-source[1];
-                                t_matrix_final[i][j] = (d_coeff*(t_matrix_final[i-1][j]+t_matrix_final[i][j-1]+t_matrix_final[i][j+1])+source[0])/d_coeff_p;
-                            }
-                            else
-                            {
-                                d_coeff_p = 3*d_coeff;
-                                t_matrix_final[i][j] = d_coeff*(t_matrix_final[i-1][j]+t_matrix_final[i][j-1]+t_matrix_final[i][j+1])/d_coeff_p;
+			}
+		}
+	}
+	else if (j == 0)
+	{
+		if (i == (rw-1))
+		{
+			cornerSource(d_bc[0],d_bc[3], source);
+			d_coeff_p = 2*d_coeff-source[1];
+			active_half[active_index] = (d_coeff*(passive_half[i-1][j] + passive_half[i][j+1])+source[0])/d_coeff_p;
+		}
+		else
+		{
+			if (d_bc[3]>0)
+			{
+				source[0] = 2*d_coeff*d_bc[3];
+				source[1] = -2*d_coeff;
+				d_coeff_p = 3*d_coeff-source[1];
+				active_half[active_index] = (d_coeff*(passive_half[i-1][j] + passive_half[i+1][j] + passive_half[i][j+1])+source[0])/d_coeff_p;
+			}
+			else
+			{
+				d_coeff_p = 3*d_coeff;
+				active_half[active_index] = d_coeff*(passive_half[i+1][j]+passive_half[i-1][j]+passive_half[i][j+1])/d_coeff_p;
 
-                            }
-                        }
-                    }
-                    else if (j == (cl-1))
-                    {
-                        if (BC[1]>0)
-                        {
-                            source[0] = 2*d_coeff*BC[1];
-                            source[1] = -2*d_coeff;
-                            d_coeff_p = 3*d_coeff-source[1];
-                            t_matrix_final[i][j] = (d_coeff*(t_matrix_final[i-1][j]+t_matrix_final[i+1][j]+t_matrix_final[i][j-1])+source[0])/d_coeff_p;
-                        }
-                        else
-                        {
-                            d_coeff_p = 3*d_coeff;
-                            t_matrix_final[i][j] = d_coeff*(t_matrix_final[i-1][j]+t_matrix_final[i+1][j]+t_matrix_final[i][j-1])/d_coeff_p;
+			}
+		}
+	}
+	else if (i == (rw-1))
+	{
+		if (j == (cl-1))
+		{
+			cornerSource(d_bc[0], d_bc[1], source);
+			d_coeff_p = 2*d_coeff-source[1];
+			active_half[active_index] = (d_coeff*(passive_half[i-1][j]+passive_half[i][j-1])+source[0])/d_coeff_p;
 
-                        }
-                    }
-                    else
-                    {
-                        d_coeff_p = 4*d_coeff;
-                        t_matrix_final[i][j] = d_coeff*(t_matrix_final[i-1][j]+t_matrix_final[i+1][j]+t_matrix_final[i][j-1]+t_matrix_final[i][j+1])/d_coeff_p;
-                    }
-                }
-            }
-        }
+		}
+		else
+		{
+			if (d_bc[0]>0)
+			{
+				source[0] = 2*d_coeff*d_bc[0];
+				source[1] = -2*d_coeff;
+				d_coeff_p = 3*d_coeff-source[1];
+				active_half[active_index] = (d_coeff*(passive_half[i-1][j]+passive_half[i][j-1]+passive_half[i][j+1])+source[0])/d_coeff_p;
+			}
+			else
+			{
+				d_coeff_p = 3*d_coeff;
+				active_half[active_index] = d_coeff*(passive_half[i-1][j]+passive_half[i][j-1]+passive_half[i][j+1])/d_coeff_p;
+
+			}
+		}
+	}
+	else if (j == (cl-1))
+	{
+		if (d_bc[1]>0)
+		{
+			source[0] = 2*d_coeff*d_bc[1];
+			source[1] = -2*d_coeff;
+			d_coeff_p = 3*d_coeff-source[1];
+			active_half[active_index] = (d_coeff*(passive_half[i-1][j]+passive_half[i+1][j]+passive_half[i][j-1])+source[0])/d_coeff_p;
+		}
+		else
+		{
+			d_coeff_p = 3*d_coeff;
+			active_half[active_index] = d_coeff*(passive_half[i-1][j]+passive_half[i+1][j]+passive_half[i][j-1])/d_coeff_p;
+
+		}
+	}
+	else
+	{
+		d_coeff_p = 4*d_coeff;
+		active_half[active_index] = d_coeff*(passive_half[i-1][j]+passive_half[i+1][j]+passive_half[i][j-1]+passive_half[i][j+1])/d_coeff_p;
+	}
+
+}
 
 
-__global__ void gaussSeidel_RB ( )
+__global__ void gaussSeidel_RB (double *red_matrix_initial, double *black_matrix_initial)
 {
-    // Initialize array of guesses.
-    double red_matrix_initial[DIVISIONS][DIVISIONS/2];
-    double red_matrix_final[DIVISIONS][DIVISIONS/2];
-    double black_matrix_initial[DIVISIONS][DIVISIONS/2];
-    double black_matrix_final[DIVISIONS][DIVISIONS/2];
 
+    // Initialize array of guesses.
     double diff_matrix;
+	double *red_matrix_final;
+	double *black_matrix_final;
     int iter = 0;
 
     // Initialize matrices.
-	red_matrix_initial[blockIdx.x][threadIdx.x] = d_guess;
-	__syncthreads();
-	red_matrix_final[blockIdx.x][threadIdx.x] = d_guess;
-	__syncthreads();
-	black_matrix_initial[blockIdx.x][threadIdx.x] = d_guess;
-	__syncthreads();
-	black_matrix_final[blockIdx.x][threadIdx.x] = d_guess;
-	__syncthreads();
+	int indx = blockIdx.x * blockDim.x + threadIdx.x;
 
+	cudaMemcpy(red_matrix_final,red_matrix_initial,sizeof(red_matrix_initial),cudaMemcpyDeviceToDevice);
+	cudaMemcpy(black_matrix_final,black_matrix_initial,sizeof(black_matrix_initial),cudaMemcpyDeviceToDevice);
 
     while (true)
     {
 
-        differencingOperation(red_matrix_final, black_matrix_final);
+		// Lets say red is in position (0,0).
+        differencingOperation(red_matrix_final, black_matrix_final,indx,1);
 		__syncthreads();
-		differencingOperation(black_matrix_final, red_matrix_final);
+		differencingOperation(black_matrix_final, red_matrix_final,indx,2);
 		__syncthreads();
 
         // Get the absolute value of the difference.
-        diff_matrix = 0;
-        for (int k = 0; k < rw; k++)
-        {
-            for (int n = 0; n < cl; n++)
-            {
-                diff_matrix += abs(t_matrix_initial[k][n] - t_matrix_final[k][n]);
 
-            }
+        diff_matrix = 0;
+        for (int k = 0; k < d_numel; k++)
+        {
+
+            diff_matrix += abs(red_matrix_initial[k] - red_matrix_final[k]) + abs(black_matrix_initial[k] - black_matrix_final[k]);
+
         }
 
         iter++;
 
-        if ((diff_matrix / (rw*cl)) < TOLERANCE)
+        if ((diff_matrix / (d_numel*2)) < TOLERANCE)
         {
             printf("The solution converges after %.d iterations.\n",iter);
 
@@ -241,16 +273,25 @@ __global__ void gaussSeidel_RB ( )
         //}
 
         // Make the initial the final.
-        for (int k = 0; k < rw; k++)
-        {
-            for (int n = 0; n < cl; n++)
-            {
-                t_matrix_initial[k][n] = t_matrix_final[k][n];
-            }
+
+		// final to initial??
+		cudaMemcpy(red_matrix_initial,red_matrix_final,sizeof(red_matrix_initial),cudaMemcpyDeviceToDevice);
+		cudaMemcpy(black_matrix_initial,black_matrix_final,sizeof(black_matrix_initial),cudaMemcpyDeviceToDevice);
+
+		__syncthreads();
+
+       /* for (int k = 0; k < rw; k++)
+		{
+
+			red_matrix_initial[k] = red_matrix_final[k];
+			black_matrix_initial[k] = black_matrix_final[k];
+            
         }
+		*/
     }
 }
 
+}
 
 int main()
 {
@@ -260,6 +301,13 @@ int main()
     double guess;
 	double coeff = TH_COND * d_A / d_ds;
     double bound_cond[4];
+	const int numel = DIVISIONS*DIVISIONS/2;
+	double red[numel];
+    double black[numel];
+	double *d_red;
+	double *d_black;
+	int rw = DIVISIONS;
+	int cl = DIVISIONS/2;
 
 
     // Get initial conditions
@@ -276,28 +324,38 @@ int main()
     cout << "Provide a guess Temperature for the slab in Kelvin:\n";
     cin >> guess;
 
-	// Initialize the CUDA put the constants in constant memory
+	for (int k = 0; k<numel; k++)
+	{
+		red[k] = guess;
+		black[k] = guess;
+	}
+
+	// Put the constants in constant memory.
 	cudaMemcpyToSymbol( d_bc, bound_cond, sizeof(bound_cond));
-	cudaMemcpyToSymbol( &d_guess, &guess, sizeof(guess)) ;
 	cudaMemcpyToSymbol( &d_A, &A, sizeof(A));
 	cudaMemcpyToSymbol( &d_ds, &ds, sizeof(ds));
-	cudaMemcpyToSymbol( &d_d_coeff, &coeff, sizeof(coeff));
+	cudaMemcpyToSymbol( &d_coeff, &coeff, sizeof(coeff));
+	cudaMemcpyToSymbol( &d_numel, &numel, sizeof(numel));
+
+	// Copy the Initial arrays to the GPU.
+	cudaMemcpy(d_red, &red, sizeof(red), cudaMemcpyHostToDevice);
+	cudaMemcpy(d_black, &black, sizeof(black), cudaMemcpyHostToDevice);
 
     double wall0 = clock();
-    gaussSeidel_RB <<< DIVISIONS,DIVISIONS/2 >>> ();
+    gaussSeidel_RB<<<rw,cl>>>(d_red,d_black);
+
+	cudaMemcpy(red,&red_matrix_final,cudaMemcpyDeviceToHost);
+	cudaMemcpy(black,&black_matrix_final,cudaMemcpyDeviceToHost);
     double wall1 = clock();
     double timed = (wall1-wall0)/double(CLOCKS_PER_SEC);
 
     printf("That took %.3e seconds.\n",timed);
 
-	double red;
-	double black;
-
-
 	cudaFree(&d_A);
 	cudaFree(&d_ds);
 	cudaFree(&d_guess);
 	cudaFree(&d_bc);
+	cudaFree(&d_coeff);
 
     return 0;
 }
